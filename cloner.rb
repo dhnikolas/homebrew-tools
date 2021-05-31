7b942578b2ed9ce170b49ee4576dd13412d252c8
Nikolay <dhnikolas@gmail.com>
2021-05-31 18:02:32 +0300

    Update cloner.rb

--- cloner.rb
+++ cloner.rb
@@ -1,8 +1,8 @@
 class Cloner < Formula
   desc "Tool for quick clone layouts repo"
   homepage "https://github.com/dhnikolas/mtsbf-cloner"
-  url "https://github.com/dhnikolas/mtsbf-cloner/releases/download/v0.0.2/cloner-0.0.2.tar.gz"
-  sha256 "7a060df245c33087bca6298aa897f9c7ea69470f22f2ced39a3c071bbaff2852"
+  url "https://github.com/dhnikolas/mtsbf-cloner/releases/download/v0.0.3/cloner-0.0.3.tar.gz"
+  sha256 "eddb748bb94747fc9436e6e1f1461cbf1e588a576949457a23395433c7ccd184"
 
   bottle :unneeded
 
