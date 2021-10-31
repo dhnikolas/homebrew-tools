082b463fb9628ffcbca7a5d9d0bc01c98d386af1
Nikolai <dhnikolas@gmail.com>
2021-10-31 02:15:46 +0000

    Deploy new cloner version v0.0.27

--- cloner.rb
+++ cloner.rb
@@ -2,8 +2,8 @@
 class Cloner < Formula
   desc "Tool for quick clone layouts repo"
   homepage "https://github.com/dhnikolas/mtsbf-cloner"
-  url "https://github.com/dhnikolas/mtsbf-cloner/releases/download/v0.0.26/cloner-v0.0.26-osx.tar.gz"
-  sha256 "a649ce8c7a49cded8babda0232f9bf7274271e989ed93253f422074f6128236a"
+  url "https://github.com/dhnikolas/mtsbf-cloner/releases/download/v0.0.27/cloner-v0.0.27-osx.tar.gz"
+  sha256 "538c61f7799722df69bb42a046af10bed7a1cdb81a670a5f2d3a72f0010b039a"
 
   def install
     out_file = File.new("#{Dir.home}/.clonerconfig", "w")
