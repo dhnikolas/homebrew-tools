280bacecc71e82623cb5c8e8aaba46c267e9c4fe
Nikolai <dhnikolas@gmail.com>
2025-04-13 17:11:42 +0000

    Deploy new warp-server version v0.0.6

--- warp-server.rb
+++ warp-server.rb
@@ -2,8 +2,8 @@
 class WarpServer < Formula
   desc "Tool for quick clone layouts repo"
   homepage "https://github.com/dhnikolas/warp-server"
-  url "https://github.com/dhnikolas/warp-server/releases/download/v0.0.5/warp-server-v0.0.5-osx.tar.gz"
-  sha256 "a74ff6f3467e78f9cec7124521b5537eb613589d87a8a263ee34005c504888a2"
+  url "https://github.com/dhnikolas/warp-server/releases/download/v0.0.6/warp-server-v0.0.6-osx.tar.gz"
+  sha256 "fb2f866d19f3f2eef7b13915d99edbf60242013d062ff0f0b8ca4e530ce5dd4c"
 
   def install
     out_file = File.new("#{Dir.home}/.warp-server.yaml", "w")
